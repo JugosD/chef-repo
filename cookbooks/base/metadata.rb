32a8521df00a5dbace5dbada9e05f9de65dadd3c
JugosD <jugosd@gmail.com>
2017-03-10 13:20:25 +0000

    Fix verison of base cookbook

--- cookbooks/base/metadata.rb
+++ cookbooks/base/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "devops@email.com"
 license          "All rights reserved"
 description      "set default for all roles"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.2"
+version          "0.1.3"
